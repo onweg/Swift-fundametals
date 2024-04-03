93ee36027b5175778bef059189418d1eb71f85c3
onweg <Innokentiydiscord@yandex.ru>
2024-04-03 18:08:04 +0300

    add login and registration screen

---
+++ Message/Message/Core/InboxView.swift
@@ -0,0 +1,20 @@
+//
+//  InboxView.swift
+//  Message
+//
+//  Created by Пользователь on 03.04.2024.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InboxView()
+    }
+}
