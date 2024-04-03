8827777c1eee19f258a220a07c0dff56e345ffd3
onweg <Innokentiydiscord@yandex.ru>
2024-04-03 19:08:34 +0300

    add inbox screen

---
+++ Message/Message/Core/Inbox/InboxView.swift
@@ -0,0 +1,55 @@
+//
+//  InboxView.swift
+//  Message
+//
+//  Created by Пользователь on 03.04.2024.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                ActiveNowView()
+                
+                List{
+                    ForEach(0 ... 10, id: \.self){message in
+                        InboxRowView()
+                    }
+                }
+                .listStyle(PlainListStyle())
+                .frame(height: UIScreen.main.bounds.height - 120)
+                
+            }
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading){
+                    HStack{
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button{
+                        print("Shoe new message view")
+                    }label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct InboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InboxView()
+    }
+}
